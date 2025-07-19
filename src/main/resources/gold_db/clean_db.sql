e23324fd16656d623a541d2afd8882859e3a6d17
Denis_Platunov <denis_platunov1@epam.com>
2025-07-19 17:21:34 +0300

    db change

--- src/main/resources/gold_db/clean_db.sql
+++ src/main/resources/gold_db/clean_db.sql
@@ -1,5 +1,5 @@
- drop table if exists databasechangeloglock;
- drop table if exists databasechangelog;
+drop table if exists databasechangeloglock;
+drop table if exists databasechangelog;
 drop table if exists property;
 drop table if exists property_category;
 drop table if exists dictionary_mapping;
