5d51b43336ffe7a8aa91de8dfe061cacbb2d5bf6
DenisPlatunov1TR <dplatunov1@findlaw.com>
2026-08-01 12:59:48 +0300

    liquibase migration

--- src/main/java/com/pladen/controller/ControllerV1.java
+++ src/main/java/com/pladen/controller/ControllerV1.java
@@ -1,15 +1,23 @@
 package com.pladen.controller;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Objects.nonNull;
 import static lombok.AccessLevel.PRIVATE;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.pladen.dto.Data;
 import com.pladen.service.ActionProcessService;
 import com.pladen.service.CommonHelper;
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
@@ -17,6 +25,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -26,6 +35,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class ControllerV1 {
     ActionProcessService actionProcessService;
     CommonHelper commonHelper;
+
+    @Autowired
+    @NonFinal
+    @Lazy
+    ControllerV1 controllerV1;
 
     public static final String BASE_PATH = "/content";
 
@@ -85,8 +99,66 @@ public class ControllerV1 {
       @PathVariable("code") String code,
       @RequestParam Map<String, String> requestParams) {
 
+    System.out.println("short " + code + " " + requestParams.toString());
+
     return actionProcessService.processActionRequest(context, code, requestParams)
         .getData();
+  }
+
+  @SneakyThrows
+  @Transactional
+  @GetMapping(BASE_PATH + "/{context}/{code}/data/short/first")
+  public @ResponseBody JsonNode actionShortDataFirst(@PathVariable("context") String context,
+      @PathVariable("code") String code,
+      @RequestParam Map<String, String> requestParams) {
+
+    System.out.println("short/index " + code + " " + requestParams.toString());
+    return actionProcessService.processActionRequest(context, code, requestParams)
+        .getData()
+        .get(0);
+  }
+
+  @SneakyThrows
+  @Transactional
+  @PostMapping(BASE_PATH + "/{context}/{code}/data/short")
+  public @ResponseBody JsonNode actionShortDataPost(@PathVariable("context") String context,
+      @PathVariable("code") String code,
+      @RequestBody Map<String, String> requestParams) {
+
+    return actionProcessService.processActionRequest(context, code, requestParams)
+        .getData();
+  }
+
+  @SneakyThrows
+  @Transactional
+  @PostMapping(BASE_PATH + "/{context}/{code}/data/short/first")
+  public @ResponseBody JsonNode actionShortDataPostByIndex(@PathVariable("context") String context,
+      @PathVariable("code") String code,
+      @RequestBody Map<String, String> requestParams) {
+
+    return actionProcessService.processActionRequest(context, code, requestParams)
+        .getData()
+        .get(0);
+  }
+
+  @SneakyThrows
+  @RequestMapping("/**")
+  public ResponseEntity<byte[]> proxyPath(HttpServletRequest request,
+      @RequestBody(required = false) byte[] body) {
+
+    System.out.println("Proxying request: " + request.getRequestURI());
+
+    final JsonNode data = controllerV1.actionShortDataFirst("system",
+        "http-proxy",
+        Map.of(
+            "uri", request.getRequestURI(),
+            "method", request.getMethod(),
+            "body", nonNull(body) ? new String(body, UTF_8) : ""
+        ));
+
+    return ResponseEntity.status(data.at("/status").asInt())
+        .contentType(APPLICATION_JSON)
+        .body(data.at("/body").asText().getBytes());
   }
 
 }
